4c307b08ae6f93c48c910c52fa68c75b9cfc9172
kokivanov <kokivanov@gmail.com>
2022-06-11 06:32:00 +0100

    2

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
+use App\Http\Controllers\MainController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\MainController;
 Route::get('/', [MainController::class, 'home']);
 Route::get('/about', [MainController::class, 'about']);
 Route::get('/review', [MainController::class, 'review']);
@@ -13,6 +13,6 @@ Route::post('/contactform', [MainController::class, 'contactform']);
 //Route::get('/user/{id}/{name}', function ($id, $name) {
  //   return 'ID: '.$id.'. Name: '.$name;
 //});
-// Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
