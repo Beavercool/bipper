eb03636aa01533d06328420b460503a79a6e6234
Beavercool <Mr.forest.jake@gmail.com>
2022-06-11 05:38:33 +0300

    trying todo

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,3 @@ Route::post('/contactform', [MainController::class, 'contactform']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/items', [ItemController::class, 'index']);
-Route::prefix('/item')->group(function(){
-    Route::post('/store',[ItemController::class, 'store']);
-    Route::put('/update' , [ItemController::class, 'update']);
-    Route::delete('/destroy', [ItemController::class, 'destroy']);
-});
